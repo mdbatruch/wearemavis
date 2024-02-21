3da4c32dc7f1f3f4f60ed2213b15d9d12dcd713e
Michael Batruch <michael.batruch@edealer.ca>
2024-02-20 23:30:33 -0500

    update iframe attr

--- app/video.tsx
+++ app/video.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Video = () => {
     return (
         <section className="bg-black p-16 w-screen flex justify-center">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/nqgSsWjSHFg?si=Z_160GTIFm74P4WZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/nqgSsWjSHFg?si=Z_160GTIFm74P4WZ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </section>
     );
 };
