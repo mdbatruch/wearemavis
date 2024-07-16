aa4f4c717080aacb7b4ec7f75ea8dd8bafc3e54c
Michael Batruch <michael.batruch@edealer.ca>
2024-07-16 11:49:53 -0400

    update single component

--- app/latest.tsx
+++ app/latest.tsx
@@ -8,14 +8,14 @@ const Latest: React.FC = () => {
         id="listen"
         className="bg-white p-16 w-full flex justify-evenly flex-col md:flex-row">
             <div className="img-container p-8 flex justify-center">
-                <Image src="/lonely_show.jpeg" width={500} height={500} alt="logo" />
+                <Image src="/carolina.png" width={500} height={500} alt="logo" />
             </div>
             <div className="text-container p-8 relative">
-                <h2 className="font-avenir font-black text-black text-6xl uppercase">Lonely Show</h2>
+                <h2 className="font-avenir font-black text-black text-6xl uppercase">Carolina</h2>
                 <p className="text-3xl text-black">new single out now</p>
                 <a
                 className="btn my-1 md:absolute bottom-0 lg:bottom-1/2 w-3/6 text-xl"
-                href="https://open.spotify.com/track/4PGZZuMJOYWfg0juq84VbC?si=5baf63bce8cb4b0d"
+                href="https://open.spotify.com/album/3C79XxzS73MYdwdXcKIu59?si=gRhOog1nR_CGnvCHGtceHQ"
                 rel="noopener noreferrer">Listen</a>
             </div>
         </div>
