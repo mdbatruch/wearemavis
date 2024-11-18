698e6aee475bfc8258fdbb2bed43076f63212799
Michael Batruch <michael.batruch@edealer.ca>
2024-11-17 21:37:25 -0500

    refactor bio with layout

--- app/latest.tsx
+++ app/latest.tsx
@@ -6,18 +6,22 @@ const Latest: React.FC = () => {
     return (
         <div
         id="listen"
-        className="bg-white p-16 w-full flex justify-evenly flex-col xl:flex-row">
-            <div className="img-container p-8 flex justify-center">
-                <Image src="/HolyShitImSorry.png" width={500} height={500} alt="logo" />
+        className="bg-white p-16 w-full">
+            <div className="flex justify-evenly flex-col xl:flex-row">
+                <div className="img-container p-8 flex justify-center">
+                    <Image src="/HolyShitImSorry.png" width={500} height={500} alt="logo" />
+                </div>
+                <div className="text-container p-8 relative xl:w-2/4">
+                    <h2 className="font-avenir font-black text-black text-6xl uppercase">Holy Shit, I&apos;m Sorry</h2>
+                    <p className="text-3xl text-black my-5">new single out now</p>
+                    <a
+                    className="btn my-1 xl:absolute bottom-0 xl:bottom-1/3 w-3/6 text-xl"
+                    href="https://open.spotify.com/track/7pTb1pVcQo67cHRanPJcOR?si=98834df758cf4f11"
+                    rel="noopener noreferrer"
+                    target="_blank">Listen</a>
+                </div>
             </div>
-            <div className="text-container p-8 relative xl:w-2/4">
-                <h2 className="font-avenir font-black text-black text-6xl uppercase">Holy Shit, I&apos;m Sorry</h2>
-                <p className="text-3xl text-black my-5">new single out now</p>
-                <a
-                className="btn my-1 bottom-0 lg:bottom-1/2 w-3/6 text-xl"
-                href="https://open.spotify.com/track/7pTb1pVcQo67cHRanPJcOR?si=98834df758cf4f11"
-                rel="noopener noreferrer"
-                target="_blank">Listen</a>
+            <div className="bio-container">
                 <p className="text-1xl text-black">
                     <br/>
                     Mavis does not care what you think of them. That much is clear with their new single, “Holy Shit, I&#39;m Sorry.” This marks their third collaboration with esteemed Canadian producer Steve Rizun (NOFX, The Flatliners), and their fourth wild swing for the bleachers. From its austere, new wave beginning, to its anthemic ending, Mavis is going on a journey and they&#39;re inviting you to get in the car.<br/><br/>
